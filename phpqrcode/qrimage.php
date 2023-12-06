dd2d93e3693289949bc0a5f47ebbc3e497c5ed29
Fortunatus Adegoke <fortunatusoluwadara@gmail.com>
2023-12-06 13:50:30 +0100

    Final commits

--- phpqrcode/qrimage.php
+++ phpqrcode/qrimage.php
@@ -71,7 +71,7 @@
             $imgW = $w + 2*$outerFrame;
             $imgH = $h + 2*$outerFrame;
             
-            $base_image =ImageCreate($imgW, $imgH);
+            $base_image = ImageCreate($imgW, $imgH);
             
             $col[0] = ImageColorAllocate($base_image,255,255,255);
             $col[1] = ImageColorAllocate($base_image,0,0,0);
@@ -86,7 +86,7 @@
                 }
             }
             
-            $target_image =ImageCreate($imgW * $pixelPerPoint, $imgH * $pixelPerPoint);
+            $target_image = ImageCreate($imgW * $pixelPerPoint, $imgH * $pixelPerPoint);
             ImageCopyResized($target_image, $base_image, 0, 0, 0, 0, $imgW * $pixelPerPoint, $imgH * $pixelPerPoint, $imgW, $imgH);
             ImageDestroy($base_image);
             
